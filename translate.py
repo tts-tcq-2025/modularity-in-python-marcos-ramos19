463f30581bfd0f36f6313aba58b29d1ad9a36397
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-10 09:31:04 -0600

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,6 +1,7 @@
+from .colors import MAJOR_COLORS, MINOR_COLORS
+
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
-
 
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
@@ -11,7 +12,6 @@ def get_color_from_pair_number(pair_number):
   if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
 
 def get_pair_number_from_color(major_color, minor_color):
   try:
