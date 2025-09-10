9456d70c8a108964fe024b94fcad70d4b2d832e0
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-10 09:34:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from colorcode.tests_module import test_number_to_pair, test_pair_to_number
+from .tests_module import test_number_to_pair, test_pair_to_number
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
