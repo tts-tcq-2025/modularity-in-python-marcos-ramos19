81c10cf5549006bdf45bbb5a60c8fa93ab2fbfe3
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-10 11:20:11 -0600

    Update manual_reference_table.py

--- manual_reference_table.py
+++ manual_reference_table.py
@@ -1,5 +1,5 @@
 from colors import MAJOR_COLORS, MINOR_COLORS
-from translate import get_color_from_pair_number
+from pair_map import get_color_from_pair_number
 
 def build_reference_table():
   total = len(MAJOR_COLORS) * len(MINOR_COLORS)
